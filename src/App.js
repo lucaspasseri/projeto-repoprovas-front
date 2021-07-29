291662d8c999992c50e93bc10a590e464d3d5e17
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-07-29 16:04:32 -0300

    Testing integration with backend.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import axios from "axios";
+import {useState} from "react";
 
 function App() {
+  const [response, setResponse] = useState("");
 
   function testingBackend(){
     const request = axios.get(`${process.env.REACT_APP_API_BASE_URL}/test`);
-    request.then(response => console.log(response));
-    request.catch(error => console.log(error));
+    request.then(response => setResponse(response.data));
+    request.catch(error => setResponse(`${error.response.status} ${error.response.statusText}`));
   }
 
   return (
     <>
       <h1>Hello world!</h1>
-      <button onClick={()=>testingBackend}>test backend</button>
+      <button onClick={()=>testingBackend()}>test backend</button>
+      <div>{response}</div>
     </>
   );
 }
