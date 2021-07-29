06083d48c646154f8481a6725233a2e673d25cfc
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-07-29 15:50:19 -0300

    Integration with backend.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import axios from "axios";
+
 function App() {
+
+  function testingBackend(){
+    const request = axios.get(`${process.env.REACT_APP_API_BASE_URL}/test`);
+    request.then(response => console.log(response));
+    request.catch(error => console.log(error));
+  }
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <button onClick={()=>testingBackend}>test backend</button>
+    </>
   );
 }
 
